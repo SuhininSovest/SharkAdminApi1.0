7db35eef2c7e9ba3f4066527eb7097e2b3579e35
Player <suhinin.sovest@yandex.ru>
2023-10-16 17:50:59 +0600

    first commit. Create repository

---
+++ src/main/java/com/example/SharkAdminApi/SharkAdminApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.SharkAdminApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkAdminApiApplication.class, args);
+		System.out.println("Hallo World!");
+	}
+
+}
---
+++ src/test/java/com/example/SharkAdminApi/SharkAdminApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.SharkAdminApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("test1");
+	}
+
+}
